35779fa23bdee28b58d83376b99b167c9a3562b4
Gisle Aas <gisle@aas.no>
2001-03-14 05:14:54 +0000

    Broken out of Digest-MD5-2.12

--- SHA1.pm
+++ SHA1.pm
@@ -1,69 +1,122 @@
 package Digest::SHA1;
 
-# This demonstrates what need to be done to conform to the same
-# interface as Digest::MD5 (and Digest::HMAC).  This should really be
-# implemented similar to how Digest::MD5 is, and SHA made into a stub.
+use strict;
+use vars qw($VERSION @ISA @EXPORT_OK);
 
-$VERSION = '0.01';  # $Date$
+$VERSION = '2.00';  # $Date$
 
-use SHA 1.2;
-
-my $sha_ver = SHA::sha_version();
-die "Wrong SHA version ($sha_ver)" unless $sha_ver eq "SHA-1";
-
-sub new {
-    my $class = shift;
-    my $self = SHA->new;
-    bless \$self, $class;
-}
-
-sub reset
-{
-    my $self = shift;
-    $$self->reset(@_);
-    $self;
-}
-
-sub add
-{
-    my $self = shift;
-    $$self->add(@_);
-    $self;
-}
-
-sub addfile
-{
-    my $self = shift;
-    $$self->addfile(@_);
-    $self;
-}
-
-sub digest
-{
-    my $self = shift;
-    $$self->digest(@_);
-}
-
-sub hexdigest
-{
-    unpack("H*", shift->digest);
-}
-
-sub b64digest
-{
-    require MIME::Base64;
-    my $digest = MIME::Base64::encode(shift->digest, '');
-    $digest =~ s/=+$//;
-    $digest;
-}
-
-# Functional interface
 require Exporter;
 *import = \&Exporter::import;
 @EXPORT_OK = qw(sha1 sha1_hex sha1_base64);
 
-sub sha1          {  Digest::SHA1->new->add(@_)->digest;    }
-sub sha1_hex      {  Digest::SHA1->new->add(@_)->hexdigest; }
-sub sha1_base64   {  Digest::SHA1->new->add(@_)->b64digest; }
+require DynaLoader;
+@ISA=qw(DynaLoader);
+Digest::SHA1->bootstrap($VERSION);
+
+*reset = \&new;
 
 1;
+__END__
+
+=head1 NAME
+
+Digest::SHA1 - Perl interface to the SHA-1 Algorithm
+
+=head1 SYNOPSIS
+
+ # Functional style
+ use Digest::SHA1  qw(sha1 sha1_hex sha1_base64);
+
+ $digest = sha1($data);
+ $digest = sha1_hex($data);
+ $digest = sha1_base64($data);
+
+
+ # OO style
+ use Digest::SHA1;
+
+ $ctx = Digest::SHA1->new;
+
+ $ctx->add($data);
+ $ctx->addfile(*FILE);
+
+ $digest = $ctx->digest;
+ $digest = $ctx->hexdigest;
+ $digest = $ctx->b64digest;
+
+=head1 DESCRIPTION
+
+The C<Digest::SHA1> module allows you to use the NIST SHA-1 message
+digest algorithm from within Perl programs.  The algorithm takes as
+input a message of arbitrary length and produces as output a 160-bit
+"fingerprint" or "message digest" of the input.
+
+The C<Digest::SHA1> module provide a procedural interface for simple
+use, as well as an object oriented interface that can handle messages
+of arbitrary length and which can read files directly.
+
+A binary digest will be 20 bytes long.  A hex digest will be 40
+characters long.  A base64 digest will be 27 characters long.
+
+
+=head1 FUNCTIONS
+
+The following functions can be exported from the C<Digest::SHA1>
+module.  No functions are exported by default.
+
+=over 4
+
+=item sha1($data,...)
+
+This function will concatenate all arguments, calculate the SHA-1
+digest of this "message", and return it in binary form.
+
+=item sha1_hex($data,...)
+
+Same as sha1(), but will return the digest in hexadecimal form.
+
+=item sha1_base64($data,...)
+
+Same as sha1(), but will return the digest as a base64 encoded string.
+
+=back
+
+=head1 METHODS
+
+The C<Digest::SHA1> module provide the standard C<Digest> OO-interface.
+The constructor looks like this:
+
+=over 4
+
+=item $sha1 = Digest->new('SHA-1')
+
+=item $sha1 = Digest::SHA1->new
+
+The constructor returns a new C<Digest::SHA1> object which encapsulate
+the state of the SHA-1 message-digest algorithm.  You can add data to
+the object and finally ask for the digest using the methods described
+in L<Digest>.
+
+=back
+
+=head1 SEE ALSO
+
+L<Digest>, L<Digest::HMAC_SHA1>, L<Digest::MD5>
+
+http://www.itl.nist.gov/fipspubs/fip180-1.htm
+
+=head1 COPYRIGHT
+
+This library is free software; you can redistribute it and/or
+modify it under the same terms as Perl itself.
+
+ Copyright 1999-2001 Gisle Aas.
+ Copyright 1997 Uwe Hollerbach.
+
+=head1 AUTHORS
+
+Peter C. Gutmann,
+Uwe Hollerbach <uh@alumni.caltech.edu>,
+Gisle Aas <gisle@aas.no>
+
+=cut
