af0872e0bb637cb361e5f7ce40660016d6ba0c3b
Gisle Aas <gisle@aas.no>
2002-12-27 09:28:11 +0000

    Release 2.02

--- SHA1.pm
+++ SHA1.pm
@@ -3,7 +3,7 @@ package Digest::SHA1;
 use strict;
 use vars qw($VERSION @ISA @EXPORT_OK);
 
-$VERSION = '2.01';  # $Date$
+$VERSION = '2.02';  # $Date$
 
 require Exporter;
 *import = \&Exporter::import;
